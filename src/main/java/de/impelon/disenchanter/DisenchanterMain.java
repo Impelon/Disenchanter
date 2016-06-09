31f99396b35fbb87cf8ed97f43f9dde2732fef6a
Impelon <ovi.tatar@googlemail.com>
2016-06-09 07:59:07 +0200

    Add files via upload

--- src/main/java/de/impelon/disenchanter/DisenchanterMain.java
+++ src/main/java/de/impelon/disenchanter/DisenchanterMain.java
@@ -1,39 +1,40 @@
 package de.impelon.disenchanter;
 
 import net.minecraftforge.common.config.Configuration;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import de.impelon.disenchanter.DisenchanterMain;
 import de.impelon.disenchanter.proxies.CommonProxy;
-import de.impelon.disenchanter.update.VersionChecker;
+import de.impelon.update.VersionChecker;
 
-@Mod(modid = "DisenchanterMod", name = "Disenchanter", version = "1.2")
+@Mod(modid="disenchanter", name="Disenchanter", version="1.2")
 public class DisenchanterMain {
-
-	public static final String MODID = "DisenchanterMod";
+	
+	public static final String MODID = "disenchanter";
 	public static final String VERSION = "1.2";
 	public static final VersionChecker versionChecker = new VersionChecker();
 	public static Configuration config;
-
-	@Mod.Instance(value = "DisenchanterMod")
+	
+	@Mod.Instance(value="disenchanter")
 	public static DisenchanterMain instance;
-
-	@SidedProxy(clientSide = "de.impelon.disenchanter.proxies.CombinedClientProxy", serverSide = "de.impelon.disenchanter.proxies.ServerProxy")
+	
+	@SidedProxy(clientSide="de.impelon.disenchanter.proxies.CombinedClientProxy", serverSide="de.impelon.disenchanter.proxies.ServerProxy")
 	public static CommonProxy proxy;
-
+	
 	@EventHandler
 	public static void preInit(FMLPreInitializationEvent ev) {
 		proxy.preInit(ev);
 	}
-
+	
 	@EventHandler
 	public static void load(FMLInitializationEvent ev) {
 		proxy.load(ev);
 	}
-
+	
 	@EventHandler
 	public static void postInit(FMLPostInitializationEvent ev) {
 		proxy.postInit(ev);
