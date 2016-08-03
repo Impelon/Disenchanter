ed138ff48f17d4b377c376066c08f330bdf40305
Impelon <ovi.tatar@googlemail.com>
2016-08-03 19:21:17 +0300

    Add files via upload

--- src/main/java/de/impelon/disenchanter/proxies/CombinedClientProxy.java
+++ src/main/java/de/impelon/disenchanter/proxies/CombinedClientProxy.java
@@ -2,6 +2,8 @@ package de.impelon.disenchanter.proxies;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -9,8 +11,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import de.impelon.disenchanter.blocks.BlockDisenchantmentTable;
-import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTableRenderer;
 import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTable;
+import de.impelon.disenchanter.gui.TileEntityDisenchantmentTableRenderer;
 import de.impelon.disenchanter.DisenchanterMain;
 
 public class CombinedClientProxy extends CommonProxy {
@@ -19,12 +21,16 @@ public class CombinedClientProxy extends CommonProxy {
 	public void preInit(FMLPreInitializationEvent ev) {
 		super.preInit(ev);
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(disenchantmentTable), 0, new ModelResourceLocation(DisenchanterMain.MODID + ":" + disenchantmentTable.getUnlocalizedName().substring(5), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(disenchantmentTable), 1, new ModelResourceLocation(DisenchanterMain.MODID + ":" + disenchantmentTable.getUnlocalizedName().substring(5), "inventory"));
 	}
 	
 	@Override
 	public void load(FMLInitializationEvent ev) {
 		super.load(ev);
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityDisenchantmentTable.class, new TileEntityDisenchantmentTableRenderer());		
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityDisenchantmentTable.class, new TileEntityDisenchantmentTableRenderer());	
+		
+		Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler((IBlockColor) disenchantmentTable, disenchantmentTable);
+		Minecraft.getMinecraft().getItemColors().registerItemColorHandler((IItemColor) itemDisenchantment, itemDisenchantment);
 	}
 	
 	@Override
