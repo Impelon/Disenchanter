8e5ae49aea0103a5c9b0007f7307fd0914ad5f16
Impelon <ovi.tatar@googlemail.com>
2016-08-03 15:07:08 +0300

    Add files via upload

--- src/main/java/de/impelon/disenchanter/proxies/CommonProxy.java
+++ src/main/java/de/impelon/disenchanter/proxies/CommonProxy.java
@@ -7,8 +7,10 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import de.impelon.disenchanter.DisenchanterMain;
 import de.impelon.disenchanter.blocks.BlockDisenchantmentTable;
-import de.impelon.disenchanter.blocks.GUIHandler;
+import de.impelon.disenchanter.blocks.ItemBlockDisenchantment;
 import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTable;
+import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTableAutomatic;
+import de.impelon.disenchanter.gui.GUIHandler;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -28,15 +30,20 @@ public class CommonProxy {
 		config.load();
 		
 		config.get("general", "CheckVersion", true, "Should Disenchanter check for new versions on startup?");
+		config.get("general", "EnableAutomaticRecipe", true, "Should the recipe for the automatic Disenchantment Table be avalible?");
 		config.get("disenchanting", "FlatDamage", 10, "How much flat damage should be dealt to Items when disenchanting?");
 		config.get("disenchanting", "MaxDurabilityDamage", 0.025, "How much of the Item's maximal durability should be dealt as damage to Items when disenchanting?");
 		config.get("disenchanting", "MaxDurabilityDamageReduceable", 0.2, "How much of the Item's maximal durability should be dealt as reduceable damage to Items when disenchanting?");
-		
+		config.get("disenchanting", "MachineDamageMultiplier", 2.5, "By how much should the dammage on the item be multiplied when using an automaic Disenchantment Table?");
+		config.get("disenchanting", "EnchantmentLossChance", 0.0, "What should the probability be of additional enchantments being lost from Items when disenchanting?");
+
 		config.save();
 		
 		GameRegistry.registerTileEntity(TileEntityDisenchantmentTable.class, "TileDisentchantmentTable");
+		GameRegistry.registerTileEntity(TileEntityDisenchantmentTableAutomatic.class, "TileDisentchantmentTableAutomatic");
+
 		
-		GameRegistry.registerBlock(disenchantmentTable, "BlockDisenchantmentTable");
+		GameRegistry.registerBlock(disenchantmentTable, ItemBlockDisenchantment.class, "BlockDisenchantmentTable");
 	}
 	
 	public void load(FMLInitializationEvent ev) {
@@ -44,8 +51,8 @@ public class CommonProxy {
 			FMLCommonHandler.instance().bus().register(DisenchanterMain.versionChecker);
 		
 		cpw.mods.fml.common.network.NetworkRegistry.INSTANCE.registerGuiHandler(DisenchanterMain.instance, new GUIHandler());
-			
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.getItemFromBlock(disenchantmentTable), 1),
+		
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.getItemFromBlock(disenchantmentTable), 1, 0),
 					"   ",
 					"YEY",
 					"ETE",
@@ -53,6 +60,18 @@ public class CommonProxy {
 					Character.valueOf('E'), Items.emerald,
 					Character.valueOf('Y'), "dyeYellow"
 		));
+		
+		if (DisenchanterMain.config.get("general", "EnableAutomaticRecipe", true).getBoolean())
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.getItemFromBlock(disenchantmentTable), 1, 1),
+					"IMI",
+					"BCB",
+					"ITI",
+					Character.valueOf('T'), new ItemStack(Item.getItemFromBlock(disenchantmentTable), 1, 0),
+					Character.valueOf('I'), "ingotIron",
+					Character.valueOf('C'), Blocks.chest,
+					Character.valueOf('B'), "dyeBlack",
+					Character.valueOf('M'), Items.comparator
+			));
 	}
 	
 	public void postInit(FMLPostInitializationEvent ev) {}
