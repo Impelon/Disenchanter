a1267d16517b7423df79f5263c91d526f2bd1940
Impelon <ovi.tatar@googlemail.com>
2016-08-03 19:21:41 +0300

    Add files via upload

--- src/main/java/de/impelon/disenchanter/blocks/BlockDisenchantmentTable.java
+++ src/main/java/de/impelon/disenchanter/blocks/BlockDisenchantmentTable.java
@@ -1,16 +1,23 @@
 package de.impelon.disenchanter.blocks;
 
+import java.util.List;
 import java.util.Random;
 
 import javax.annotation.Nullable;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.inventory.Container;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
@@ -25,9 +32,10 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import de.impelon.disenchanter.DisenchanterMain;
 
-public class BlockDisenchantmentTable extends BlockContainer {
+public class BlockDisenchantmentTable extends BlockContainer implements IBlockColor {
     
     protected static final AxisAlignedBB AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.75D, 1.0D);
+	public static final PropertyBool AUTOMATIC = PropertyBool.create("automatic");
 	
 	public BlockDisenchantmentTable() {
 		super(Material.ROCK);
@@ -36,6 +44,7 @@ public class BlockDisenchantmentTable extends BlockContainer {
 		this.setUnlocalizedName("disenchantmentTable");
 		this.setHardness(5.0F);
 		this.setResistance(2000.0F);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(AUTOMATIC, false));
 	}
 
 
@@ -93,6 +102,53 @@ public class BlockDisenchantmentTable extends BlockContainer {
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
 	}
+		
+	@Override
+	protected BlockStateContainer createBlockState() {
+	    return new BlockStateContainer(this, new IProperty[] { AUTOMATIC });
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int metadata) {
+	    return getDefaultState().withProperty(AUTOMATIC, metadata == 0 ? false : true);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+	    return state.getValue(AUTOMATIC) ? 1 : 0;
+	}
+	
+	@Override
+	public int damageDropped(IBlockState state) {
+	    return getMetaFromState(state);
+	}
+	
+	@Override
+	public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+		subItems.add(new ItemStack(itemIn, 1, 0));
+		subItems.add(new ItemStack(itemIn, 1, 1));
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public int colorMultiplier(IBlockState state, IBlockAccess access, BlockPos pos, int tintIndex) {
+		if (state.getValue(AUTOMATIC) == true)
+			return 0x888888;
+		return 0xffffff;
+	}
+	
+	@Override
+	public boolean hasComparatorInputOverride(IBlockState state) {
+		return true;
+	}
+	
+	@Override
+	public int getComparatorInputOverride(IBlockState state, World w, BlockPos pos) {
+		TileEntity te = w.getTileEntity(pos);
+		if (te instanceof TileEntityDisenchantmentTableAutomatic) 
+			return Container.calcRedstone(te);
+		return 0;
+	}
 
 	@Override
 	public boolean onBlockActivated(World w, BlockPos pos, IBlockState state, EntityPlayer p, EnumHand hand, 
@@ -112,6 +168,8 @@ public class BlockDisenchantmentTable extends BlockContainer {
 	
 	@Override
 	public TileEntity createNewTileEntity(World w, int metadata) {
+		if (metadata == 1)
+			return new TileEntityDisenchantmentTableAutomatic();
 		return new TileEntityDisenchantmentTable();
 	}
 
