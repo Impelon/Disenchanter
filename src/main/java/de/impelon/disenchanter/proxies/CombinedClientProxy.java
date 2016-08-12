5ab059e98fe304a9e346dee10ae8445c1d11040b
Impelon <ovi.tatar@googlemail.com>
2016-08-12 15:26:38 +0300

    Add files via upload

--- src/main/java/de/impelon/disenchanter/proxies/CombinedClientProxy.java
+++ src/main/java/de/impelon/disenchanter/proxies/CombinedClientProxy.java
@@ -1,15 +1,21 @@
 package de.impelon.disenchanter.proxies;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import de.impelon.disenchanter.blocks.BlockDisenchantmentTable;
 import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTable;
 import de.impelon.disenchanter.gui.TileEntityDisenchantmentTableRenderer;
@@ -29,8 +35,24 @@ public class CombinedClientProxy extends CommonProxy {
 		super.load(ev);
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityDisenchantmentTable.class, new TileEntityDisenchantmentTableRenderer());	
 		
-		Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler((IBlockColor) disenchantmentTable, disenchantmentTable);
-		Minecraft.getMinecraft().getItemColors().registerItemColorHandler((IItemColor) itemDisenchantment, itemDisenchantment);
+		Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler(new IBlockColor() {
+			
+			@Override
+			public int colorMultiplier(IBlockState state, IBlockAccess access, BlockPos pos, int tintIndex) {
+				if (state.getValue(BlockDisenchantmentTable.AUTOMATIC) == true)
+					return 0x888888;
+				return 0xffffff;
+			}
+		}, disenchantmentTable);
+		Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new IItemColor() {
+			
+			@Override
+			public int getColorFromItemstack(ItemStack stack, int tintIndex) {
+				if (stack.getItemDamage() == 1)
+					return 0x888888;
+				return 0xffffff;
+			}
+		}, itemDisenchantment);
 	}
 	
 	@Override
