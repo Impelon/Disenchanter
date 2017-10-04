84a40c0b532109b49012a54f38fc26d76004ce06
Ovi T. (Impelon) <ovi.tatar@googlemail.com>
2017-10-04 09:44:17 +0200

    Update to 1.12

--- src/main/java/de/impelon/disenchanter/gui/TileEntityDisenchantmentTableRenderer.java
+++ src/main/java/de/impelon/disenchanter/gui/TileEntityDisenchantmentTableRenderer.java
@@ -1,30 +1,31 @@
 package de.impelon.disenchanter.gui;
 
 import net.minecraft.client.model.ModelBook;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityEnchantmentTable;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.ResourceLocation;
-
-import org.lwjgl.opengl.GL11;
 
 import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTable;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class TileEntityDisenchantmentTableRenderer extends TileEntitySpecialRenderer {
+public class TileEntityDisenchantmentTableRenderer extends TileEntitySpecialRenderer<TileEntityDisenchantmentTable> {
 	
 	private static final ResourceLocation bookResource = new ResourceLocation("textures/entity/enchanting_table_book.png");
 	private ModelBook bookModel = new ModelBook();
-
-	public void renderTileEntityAt(TileEntityDisenchantmentTable tileentity,
-			double x, double y, double z, float partialTicks, int destroyStage) {
-		GL11.glPushMatrix();
-		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.15F, (float) z + 0.5F);
+	
+	@Override
+	public void render(TileEntityDisenchantmentTable tileentity,
+			double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+		GlStateManager.pushMatrix();
+		GlStateManager.translate((float) x + 0.5F, (float) y + 1.15F, (float) z + 0.5F);
 		float f1 = (float) tileentity.tickCount + partialTicks;
-		GL11.glTranslatef(0.0F, 0.1F + MathHelper.sin(f1 * 0.1F) * 0.01F, 0.0F);
+		GlStateManager.translate(0.0F, 0.1F + MathHelper.sin(f1 * 0.1F) * 0.01F, 0.0F);
 		
 		float f2;
 
@@ -34,8 +35,8 @@ public class TileEntityDisenchantmentTableRenderer extends TileEntitySpecialRend
 			f2 += ((float) Math.PI * 2F);
 
 		float f3 = tileentity.bookRotationPrev + f2 * partialTicks;
-		GL11.glRotatef(-f3 * 180.0F / (float) Math.PI, 0.0F, 1.0F, 0.0F);
-		GL11.glRotatef(-80.0F, 0.0F, 0.0F, 1.0F);
+		GlStateManager.rotate(-f3 * 180.0F / (float) Math.PI, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(-80.0F, 0.0F, 0.0F, 1.0F);
 		this.bindTexture(bookResource);
 		float f4 = tileentity.pageFlipPrev + (tileentity.pageFlip - tileentity.pageFlipPrev) * partialTicks + 0.25F;
 		float f5 = f4 + 0.5F;
@@ -53,12 +54,9 @@ public class TileEntityDisenchantmentTableRenderer extends TileEntitySpecialRend
 			f5 = 1.0F;
 
 		float f6 = tileentity.bookSpreadPrev + (tileentity.bookSpread - tileentity.bookSpreadPrev) * partialTicks;
-		GL11.glEnable(GL11.GL_CULL_FACE);
+		GlStateManager.enableCull();
 		this.bookModel.render((Entity) null, f1, f5, f4, f6, 0.0F, 0.0625F);
-		GL11.glPopMatrix();
+		GlStateManager.popMatrix();
 	}
 
-	public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float partialTicks, int destroyStage) {
-		this.renderTileEntityAt((TileEntityDisenchantmentTable) tileentity, x, y, z, partialTicks, destroyStage);
-	}
 }
