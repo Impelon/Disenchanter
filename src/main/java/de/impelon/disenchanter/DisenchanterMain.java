13bbe6ba355d31b8ebeef35cb39c8fa768d4475e
Ovi T. (Impelon) <ovi.tatar@googlemail.com>
2018-03-22 09:12:42 +0100

    Less redundancy
    - Removed redundant mod specifications

--- src/main/java/de/impelon/disenchanter/DisenchanterMain.java
+++ src/main/java/de/impelon/disenchanter/DisenchanterMain.java
@@ -1,5 +1,6 @@
 package de.impelon.disenchanter;
 
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -11,20 +12,21 @@ import de.impelon.disenchanter.DisenchanterMain;
 import de.impelon.disenchanter.proxies.CommonProxy;
 import de.impelon.disenchanter.update.VersionChecker;
 
-@Mod(modid="disenchanter", name="Disenchanter", version="1.5")
+@Mod(modid = DisenchanterMain.MODID, name = DisenchanterMain.NAME, version = DisenchanterMain.VERSION)
 public class DisenchanterMain {
 	
+	public static final String NAME = "Disenchanter";
 	public static final String MODID = "disenchanter";
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.6";
 	public static final String PREFIX = EnumChatFormatting.GRAY + "[" + EnumChatFormatting.GOLD + EnumChatFormatting.BOLD + 
-			"Disenchanter" + EnumChatFormatting.GRAY + "] " + EnumChatFormatting.RESET;
+			NAME + EnumChatFormatting.GRAY + "] " + EnumChatFormatting.RESET;
 	public static final VersionChecker versionChecker = new VersionChecker();
 	public static Configuration config;
-	
-	@Mod.Instance(value="disenchanter")
+
+	@Mod.Instance(value = MODID)
 	public static DisenchanterMain instance;
 	
-	@SidedProxy(clientSide="de.impelon.disenchanter.proxies.CombinedClientProxy", serverSide="de.impelon.disenchanter.proxies.ServerProxy")
+	@SidedProxy(clientSide = "de.impelon.disenchanter.proxies.CombinedClientProxy", serverSide = "de.impelon.disenchanter.proxies.ServerProxy")
 	public static CommonProxy proxy;
 	
 	@EventHandler
